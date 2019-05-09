980fed9ecdecb8f37d17638b98509c65f9ef87e0
frloga01 <franlobogarcia@gmail.com>
2019-05-09 15:32:40 +0200

    Subo proyecto

---
+++ ExamenVerde/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExamenVerde
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
