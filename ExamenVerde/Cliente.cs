fb6a0fb455f07e22fe42234ad4e13c899c2588f7
frloga01 <franlobogarcia@gmail.com>
2019-05-09 15:48:30 +0200

    atributos añadidos con comentarios

--- ExamenVerde/Cliente.cs
+++ ExamenVerde/Cliente.cs
@@ -6,5 +6,14 @@ namespace ExamenVerde
 {
     class Cliente
     {
+        /// <summary>
+        /// Atributos de la clase Cliente
+        /// </summary>
+        private string nombre;
+        private string dni;
+        private double saldo = 100;
+
+
+
     }
 }
